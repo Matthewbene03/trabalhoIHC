2b24721c8867b8c1fd946a84d7734d2b06abab02
Matheus Benevenuto <matthewbene03@gmail.com>
2025-09-29 18:12:57 -0300

    Atualizações nos botões de dar zoom.

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -3,12 +3,19 @@ let body = document.getElementById('body');
 let h1 = document.querySelector('h1');
 let h2 = document.querySelectorAll('h2');
 let paragrafos = document.querySelectorAll('p');
+let imagens = document.querySelectorAll(".sectionImg img");
 
 const incremento = 10;
 const mAX_H = 150;
 const mAX_P = 60;
 const min_H = 60;
 const min_P = 20;
+const incrementoImg = 20;
+const maxImg = 700;
+const minImg = 200;
+
+let html = document.documentElement;
+let base = 16; // valor inicial
 
 function mudarContraste() {
     navMenu.classList.toggle("navMenuBlack");
@@ -20,67 +27,11 @@ function mudarContraste() {
 }
 
 function aumentarFonteTexto() {
-    if (parseFloat(window.getComputedStyle(h1).fontSize) === mAX_H) {
-        console.log("h1 chegou no limite");
-    } else {
-        const novoTamanho = Math.min((parseFloat(window.getComputedStyle(h1).fontSize) + incremento), mAX_H);
-        h1.style.fontSize = `${novoTamanho}px`;
-        console.log(window.getComputedStyle(h1).fontSize);
-    }
-
-    h2.forEach(el => {
-        if (parseFloat(window.getComputedStyle(el).fontSize) === mAX_H) {
-            console.log("h2 chegou no limite");
-        } else {
-            const novoTamanho = Math.min((parseFloat(window.getComputedStyle(el).fontSize) + incremento), mAX_H);
-            el.style.fontSize = `${novoTamanho}px`;
-            console.log(window.getComputedStyle(el).fontSize);
-        }
-    });
-
-
-    paragrafos.forEach(el => {
-        if (parseFloat(window.getComputedStyle(el).fontSize) === mAX_P) {
-            console.log("p chegou no limite");
-        } else {
-            const novoTamanho = Math.min((parseFloat(window.getComputedStyle(el).fontSize) + incremento), mAX_P);
-            el.style.fontSize = `${novoTamanho}px`;
-            console.log(window.getComputedStyle(el).fontSize);
-        }
-    });
-
-    console.log("Aumentou!")
+    base = Math.min(base + 2, 30); // limite de 30px base
+    html.style.fontSize = base + "px";
 }
 
 function diminuirFonteTexto() {
-    if (parseFloat(window.getComputedStyle(h1).fontSize) === min_H) {
-        console.log("h1 chegou no limite minimo");
-    } else {
-        const novoTamanho = Math.max((parseFloat(window.getComputedStyle(h1).fontSize) - incremento), min_H);
-        h1.style.fontSize = `${novoTamanho}px`;
-        console.log(window.getComputedStyle(h1).fontSize);
-    }
-
-    h2.forEach(el => {
-        if (parseFloat(window.getComputedStyle(el).fontSize) === min_H) {
-            console.log("h2 chegou no limite minimo");
-        } else {
-            const novoTamanho = Math.max((parseFloat(window.getComputedStyle(el).fontSize) - incremento), min_H);
-            el.style.fontSize = `${novoTamanho}px`;
-            console.log(window.getComputedStyle(el).fontSize);
-        }
-    });
-
-
-    paragrafos.forEach(el => {
-        if (parseFloat(window.getComputedStyle(el).fontSize) === min_P) {
-            console.log("p chegou no limite minimo");
-        } else {
-            const novoTamanho = Math.max((parseFloat(window.getComputedStyle(el).fontSize) - incremento), min_P);
-            el.style.fontSize = `${novoTamanho}px`;
-            console.log(window.getComputedStyle(el).fontSize);
-        }
-    });
-
-    console.log("Diminuiu!")
+    base = Math.max(base - 2, 10); // limite mínimo 10px base
+    html.style.fontSize = base + "px";
 }
