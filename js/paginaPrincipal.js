0e71ffba01ba417ada0da14e779f0e701a13a7be
Matheus Benevenuto <matthewbene03@gmail.com>
2025-09-29 21:37:01 -0300

    Atualizações

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -1,7 +1,5 @@
 let navMenu = document.getElementById('navMenuPrincipal');
 let body = document.getElementById('body');
-let h1 = document.querySelector('h1');
-let h2 = document.querySelectorAll('h2');
 let paragrafos = document.querySelectorAll('p');
 let imagens = document.querySelectorAll(".sectionImg img");
 
@@ -21,8 +19,6 @@ function mudarContraste() {
     navMenu.classList.toggle("navMenuBlack");
     body.classList.toggle('bodyBlack');
     document.body.classList.toggle("high-contrast");
-    h1.classList.toggle('tituloBlack');
-    document.h2.classList.toggle('tituloBlack');
     console.log("Mudou!")
 }
 
